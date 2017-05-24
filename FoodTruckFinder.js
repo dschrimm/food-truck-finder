2221d9a18532da137e8341bf94ac1811242c4ffc
Schrimmer <danisch@amazon.com>
2017-05-24 13:34:03 -0700

    Handle error

--- FoodTruckFinder.js
+++ FoodTruckFinder.js
@@ -10,6 +10,10 @@ getOpenFoodTrucks = function() {
     var endTimeQuery = '&$where=end24>=\'' + currentTime + '\'' + ' AND start24<=\''+ currentTime + '\'';
 
     request('http://data.sfgov.org/resource/bbb8-hzi6.json?' + dateQuery + endTimeQuery, function (error, response, body) {
-        console.log(body);
+        if (error || JSON.parse(body).error) {
+            console.log('Sorry, something went wrong and we couldn\'t get the list of open food trucks for you.');
+        } else {
+            console.log(body);
+        }
     });
 }();
