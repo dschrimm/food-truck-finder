08f125ac4da1225fa43b01be634826e6a6986567
Schrimmer <danisch@amazon.com>
2017-05-26 10:09:41 -0700

    Print first ten open trucks in alphabetical ascending order in column format

--- FoodTruckFinder.js
+++ FoodTruckFinder.js
@@ -1,19 +1,34 @@
 var request = require('request');
+var columnify = require('columnify');
 
-getOpenFoodTrucks = function() {
+getOpenFoodTrucks = function(offset) {
     // Assumes you are in the same region as SF
     var currentDate = new Date();
     var currentDay = currentDate.getDay();
-    var currentTime = currentDate.getHours() + ':' + ('0'+currentDate.getMinutes()).slice(-2);
+
+    // Configure time in 24-hour format
+    var currentTime = ('0' + currentDate.getHours()).slice(-2) + ':' + ('0' + currentDate.getMinutes()).slice(-2);
 
     var dateQuery = 'dayorder=' + currentDay;
     var endTimeQuery = '&$where=end24>=\'' + currentTime + '\'' + ' AND start24<=\''+ currentTime + '\'';
 
-    request('http://data.sfgov.org/resource/bbb8-hzi6.json?' + dateQuery + endTimeQuery, function (error, response, body) {
-        if (error || JSON.parse(body).error) {
+    var order = '&$order=applicant ASC';
+    var limit = '&$limit=10';
+    // offset = '&$offset=' + offset * 10;
+
+    request('http://data.sfgov.org/resource/bbb8-hzi6.json?' + dateQuery + endTimeQuery + order + limit, function (error, response, body) {
+        body = JSON.parse(body);
+        if (error || body.error) {
             console.log('Sorry, something went wrong and we couldn\'t get the list of open food trucks for you.');
         } else {
-            console.log(body);
+            var trucks = [];
+            for(i=0; i<body.length; i++) {
+                trucks.push({
+                    applicant: body[i].applicant,
+                    location: body[i].location
+                });
+            }
+            console.log(columnify(trucks));
         }
     });
 }();
